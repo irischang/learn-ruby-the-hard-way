266b6fc91fd490f78387bf387a6ad01d4fdd7c1f
Alex Stokes <r.alex.stokes@gmail.com>
2013-10-31 15:56:29 -0400

    first exercise

---
+++ ex1.rb
@@ -0,0 +1,20 @@
+a = 2 # print line 2
+
+case a
+when 1
+    puts "Hello, world!"
+when 2
+    puts "Hello, again"
+when 3
+    puts 'I like typing this.'
+when 4
+    puts "This is fun."
+when 5
+    puts 'Yay! Printing.'
+when 6
+    puts "I'd rather you much 'not'"
+when 7
+    puts 'I "said" do not touch this.'
+when 8
+    puts 'This is another line'
+end
