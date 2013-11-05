8a231df26a5e15243ccb3ac46163ff645aa45ac7
Alex Stokes <r.alex.stokes@gmail.com>
2013-11-04 22:03:19 -0600

    added Sinatra sessions example

---
+++ ex52.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'sinatra'
+
+use Rack::Session::Pool
+
+get '/count' do 
+    session[:count] ||= 0
+    session[:count] += 1
+    "Count: #{session.inspect}"
+end
+
+get '/reset' do 
+    session.clear
+    "Count reset to 0."
+end
